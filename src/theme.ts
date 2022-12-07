e22dc75196d7a7b4db2697c3a6fef38e474c9af4
Sijun <sionwer4@naver.com>
2022-12-07 09:35:36 +0900

    feat: theme add

---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme:DefaultTheme = {
+    bgColor:"white",
+    textColor:"black",
+    buttonColor:"tomato",
+}
+
+export const darkTheme:DefaultTheme ={
+    bgColor:"black",
+    textColor:"white",
+    buttonColor:"teal",
+}
