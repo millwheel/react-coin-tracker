f22a69ff71c697d72588187a6bc2a693e0695348
Sijun <sionwer4@naver.com>
2022-12-08 16:34:40 +0900

    feat:react query add

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,21 @@
+import { QueryClient, QueryClientProvider } from "react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
-import { darkTheme, lightTheme } from './theme';
+import { darkTheme, lightTheme } from "./theme";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <div>
-    <ThemeProvider theme={darkTheme}>
-      <App />
-    </ThemeProvider>
-  </div>
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={darkTheme}>
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
 );
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -4,10 +4,12 @@ import { useState, useEffect } from "react";
 
 const Container = styled.div`
   padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
 `;
 
 const Header = styled.header`
-  height: 10vh;
+  height: 15vh;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -19,10 +21,12 @@ const Coin = styled.li`
   background-color: white;
   color: ${(props) => props.theme.bgColor};
   margin-bottom: 10px;
-  padding: 20px;
+  padding: 10px;
   border-radius: 15px;
   a {
-    padding: 20px;
+    display: flex;
+    align-items: center;
+    padding: 10px;
     transition: color 0.2s ease-in-out;
     display: block;
   }
@@ -40,7 +44,14 @@ const Title = styled.h1`
 
 const Loader = styled.span`
   text-align: center;
+  display: block;
 `;
+
+const Img = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
+`
 
 interface CoinInterface {
   id: string;
@@ -74,9 +85,13 @@ function Coins() {
         <CoinList>
           {coins.map((coin) => (
             <Coin key={coin.id}>
-              <Link to={`/${coin.id}`}>
-                <img src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase}`} />
-                {coin.name} &rarr;</Link>
+              <Link to={{
+                pathname:`/${coin.id}`,
+                state:{ name: coin.name },
+              }}>
+                <Img src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`} />
+                {coin.name} &rarr;
+              </Link>
             </Coin>
           ))}
         </CoinList>
